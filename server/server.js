ed86f24db05413e8b71faafe44a84234f43308e9
kyle-mucerino <kyle.mucerino@gmail.com>
2023-06-08 12:03:30 -0700

    initial commit (again)

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const userRoutes = require("./routes/user-routes");
+const imageRoutes = require("./routes/image-upload"); // add this file import to the top of the file
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+// app.use(require('./routes'));
+app.use("/api/", userRoutes);
+app.use("/api/", imageRoutes); // add this route for image upload
+
+// express middleware, used to be bodyparser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
